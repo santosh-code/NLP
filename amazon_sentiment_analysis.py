8ce08d46d4e66f67a02673948f91535cad71923b
santoshkumar <68806441+santosh-code@users.noreply.github.com>
2020-12-04 11:23:32 +0530

    Add files via upload

---
+++ amazon_sentiment_analysis.py
@@ -0,0 +1,105 @@
+import requests   # Importing requests to extract content from a url
+from bs4 import BeautifulSoup as bs # Beautifulsoup is for web scrapping...used to scrap specific content 
+import re 
+from nltk.corpus import stopwords
+#import nltk
+#from nltk.corpus import stopwords
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+# creating empty reviews list 
+phone_reviews=[]
+#forest = ["the","king","of","jungle"]
+
+for i in range(1,100):
+  ip=[]  
+  url="https://www.amazon.in/Redmi-Note-Neptune-Blue-Storage/product-reviews/B07X1KT6LD/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber="+str(i)
+  response = requests.get(url)
+  soup = bs(response.content,"html.parser")# creating soup object to iterate over the extracted content 
+  reviews = soup.findAll("span",attrs={"class","a-size-base review-text review-text-content"})# Extracting the content under specific tags  
+  for i in range(len(reviews)):
+    ip.append(reviews[i].text)  
+    phone_reviews=phone_reviews+ip  # adding the reviews of one page to empty list which in future contains all the reviews
+
+# writng reviews in a text file 
+with open("redmi_neptune.txt","w",encoding='utf8') as output:
+    output.write(str(phone_reviews))
+    
+    
+    
+# Joinining all the reviews into single paragraph 
+ip_rev_string = " ".join(phone_reviews)
+
+
+
+# Removing unwanted symbols incase if exists
+ip_rev_string = re.sub("[^A-Za-z" "]+"," ",ip_rev_string).lower()
+ip_rev_string = re.sub("[0-9" "]+"," ",ip_rev_string)
+
+
+
+# words that contained in iphone 7 reviews
+ip_reviews_words = ip_rev_string.split(" ")
+
+stop_words = stopwords.words('english')
+
+
+
+ip_reviews_words = [w for w in ip_reviews_words if not w in stop_words]
+
+
+
+# Joinining all the reviews into single paragraph 
+ip_rev_string = " ".join(ip_reviews_words)
+
+# WordCloud can be performed on the string inputs. That is the reason we have combined 
+# entire reviews into single paragraph
+# Simple word cloud
+
+
+wordcloud_ip = WordCloud(
+                      background_color='black',
+                      width=1800,
+                      height=1400
+                     ).generate(ip_rev_string)
+
+plt.imshow(wordcloud_ip)
+
+# positive words # Choose the path for +ve words stored in system
+with open('C:/Users/USER/positive_words123.txt',"r") as pos:
+  poswords = pos.read().split("\n")
+  
+poswords = poswords[36:]
+
+
+with open('C:/Users/USER/negative_words.txt',"r") as neg:
+  negwords = neg.read().split("\n")
+  
+
+
+negwords = negwords[37:]
+
+
+# negative word cloud
+# Choosing the only words which are present in negwords
+ip_neg_in_neg = " ".join ([w for w in ip_reviews_words if w in negwords])
+
+wordcloud_neg_in_neg = WordCloud(
+                      background_color='black',
+                      width=1800,
+                      height=1400
+                     ).generate(ip_neg_in_neg)
+
+plt.imshow(wordcloud_neg_in_neg)
+
+# Positive word cloud
+# Choosing the only words which are present in positive words
+ip_pos_in_pos = " ".join ([w for w in ip_reviews_words if w in poswords])
+wordcloud_pos_in_pos = WordCloud(
+                      background_color='black',
+                      width=1800,
+                      height=1400
+                     ).generate(ip_pos_in_pos)
+
+plt.imshow(wordcloud_pos_in_pos)
